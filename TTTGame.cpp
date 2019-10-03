819d2ef71771d940f06b0daf4b711bb37a4333e6
Steven <parrsm18@wfu.edu>
2019-10-03 16:19:29 -0400

    Added code to check for valid choices and valid games selection

--- TTTGame.cpp
+++ TTTGame.cpp
@@ -17,14 +17,36 @@ int main() {
     int selection;
     int row;
     int column;
+    bool validGuess = false;
+    bool validSelection = false;
     cout << R"(Enter "1" to play 2 player game or "2" to play against computer)" << endl;
     cin >> selection;
+    while (!validSelection){
+        if (selection != 1 && selection != 2){
+            cout << "Please enter a valid selection 1 or 2" << endl;
+            cin >> selection;
+        }
+        else {
+            validSelection = true;
+        }
+    }
+
     if (selection == 1){ // 2 Player Game
         while(game.checkForWin() == false) {
             game.printBoard();
             cout << "Player: " << game.getCurrentPlayerNumber() << " Enter row and column of guess" << endl;
             cin >> row >> column;
+            while (!validGuess) {
+                if (row > 2 || row < 0 || column > 2 || row < 0) {
+                    cout << "Please enter a valid choice (Rows 0-2, Columns 0-2)" << endl;
+                    cin >> row >> column;
+                }
+                else {
+                    validGuess = true;
+                }
+            }
             game.placeMark(row, column);
+
             if (game.checkForWin() == true){
                 cout << "Player " << game.getCurrentPlayerNumber() << " has won!" << endl;
                 game.printBoard();
